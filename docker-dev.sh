f16ec6061d85209ee57ba891c3eac120724e58ce
FutuerBeomsoo <pbs94@futuredrive.net>
2026-01-28 15:16:24 +0900

    test code update

--- docker-dev.sh
+++ docker-dev.sh
@@ -7,7 +7,7 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd "$DIR"
 
-IMAGE_NAME="openpilot-dev:configured"
+IMAGE_NAME="openpilot-dev:confi_v0.1.1"
 CONTAINER_NAME="openpilot-dev"
 
 # Host path to mount
